1f583ceb92e8fe37382df7e89de28805996d5fa3
Reid Andrew Barden <me@reidbarden.com>
2017-11-23 10:32:32 -0500

    adds load to MM interface

--- src/memory/MemoryManager.java
+++ src/memory/MemoryManager.java
@@ -1,5 +1,6 @@
 package memory;
 
+import hardware.CPU;
 import process.Process;
 
 public interface MemoryManager {
@@ -18,6 +19,15 @@ public interface MemoryManager {
      * @return true if de-allocation was successful
      */
     boolean deallocate(Process process);
+
+    /**
+     * Ensures a process is in memory and loads it into a CPU's cache and registers
+     *
+     * @param process process to load
+     * @param cpu the CPU on which to load the process
+     * @return true if loading was successful
+     */
+    boolean load(Process process, CPU cpu);
 
     /**
      * Returns the amount of free memory in the system
